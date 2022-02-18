99f1a5a926076e8c4123d2698d312c0c70c62220
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2022-02-17 19:04:27 -0500

    chromefill: add polyfill for fromEntries

--- polyfills.js
+++ polyfills.js
@@ -1,4 +1,5 @@
-// https://mathiasbynens.be/notes/globalthis
+// globalThis: https://mathiasbynens.be/notes/globalthis
+// fromEntries: https://stackoverflow.com/a/68655198/
 var actualCode = `
 (function() {
 	if (typeof globalThis === 'object') return;
@@ -11,6 +12,14 @@ var actualCode = `
 	__magic__.globalThis = __magic__;
 	delete Object.prototype.__magic__;
 }());
+
+function fromEntries(entries){
+    var res = {};
+    for(var i = 0; i < entries.length; i++) res[entries[i][0]] = entries[i][1];
+    return res;
+}
+if(!Object.fromEntries) Object.fromEntries = fromEntries;
+
 `;
 
 var script = document.createElement('script');
