a0cfe79050b1f3d33ce25d9e8d8187f1e332f4d4
InterLinked1 <24227567+InterLinked1@users.noreply.github.com>
2022-03-18 06:02:02 -0500

    chromefill: incorporated additions by VistaLover
    Adds four more polyfills:
    Promise.any (implemented in Chrome 85)
    Promise.allSettled (implemented in Chrome 76)
    String.replaceAll (implemented in Chrome 85)
    replaceChildren (implemented in Chrome 86)

--- polyfills.js
+++ polyfills.js
@@ -1,7 +1,7 @@
-// globalThis: https://mathiasbynens.be/notes/globalthis
-// fromEntries: https://stackoverflow.com/a/68655198/
-// queueMicroTask: https://stackoverflow.com/a/61605098/
 var actualCode = `
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis
+// implemented in Chrome 71
+// https://mathiasbynens.be/notes/globalthis
 (function() {
 	if (typeof globalThis === 'object') return;
 	Object.defineProperty(Object.prototype, '__magic__', {
@@ -13,19 +13,65 @@ var actualCode = `
 	__magic__.globalThis = __magic__;
 	delete Object.prototype.__magic__;
 }());
-
-function fromEntries(entries){
-    var res = {};
-    for(var i = 0; i < entries.length; i++) res[entries[i][0]] = entries[i][1];
-    return res;
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries
+// implemented in Chrome 73
+// https://stackoverflow.com/a/68655198
+// https://gitlab.com/moongoal/js-polyfill-object.fromentries/-/blob/master/index.js
+// -> https://vanillajstoolkit.com/polyfills/objectfromentries/
+if (!Object.fromEntries) {
+	Object.fromEntries = function (entries) {
+		if (!entries || !entries[Symbol.iterator]) { 
+      throw new Error('Object.fromEntries() requires a single iterable argument');
+    }
+		let obj = {};
+		for (let [key, value] of entries) {
+			obj[key] = value;
+		}
+		return obj;
+	};
 }
-if(!Object.fromEntries) Object.fromEntries = fromEntries;
-
-
-
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/any
+// implemented in Chrome 85 
+// https://github.com/ungap/promise-any
+// copied from github-wc-polyfill
+if (!('any' in Promise && typeof Promise.any == 'function')) Promise.any = function($) {
+  return new Promise(function(D, E, A, L) {
+    A = [];
+    L = $.map(function($, i) {
+      return Promise.resolve($).then(D, function(O) {
+        return ((A[i] = O), --L) || E({
+          errors: A
+        });
+      });
+    }).length;
+  });
+};
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled
+// implemented in Chrome 76
+// https://95yashsharma.medium.com/polyfill-for-promise-allsettled-965f9f2a003
+if (!('allSettled' in Promise && typeof Promise.allSettled == 'function')) Promise.allSettled = function (promises) {
+  let mappedPromises = promises.map((p) => {
+    return p
+      .then((value) => {
+        return {
+          status: 'fulfilled',
+          value,
+        };
+      })
+      .catch((reason) => {
+        return {
+          status: 'rejected',
+          reason,
+        };
+      });
+  });
+  return Promise.all(mappedPromises);
+};
+// https://developer.mozilla.org/en-US/docs/Web/API/queueMicrotask
+// implemented in Chrome 71
+// https://stackoverflow.com/a/61569775
 (function() {
 'use strict';
-
 // lazy get globalThis, there might be better ways
 const globalObj = typeof globalThis === "object" ? globalThis :
   typeof global === "object" ? global :
@@ -36,48 +82,79 @@ const globalObj = typeof globalThis === "object" ? globalThis :
 if (typeof queueMicrotask !== "function") {
 
   const checkIsCallable = (callback) => {
-    if( typeof callback !== 'function' ) {
-      throw new TypeError( "Failed to execute 'queueMicrotask': the callback provided as parameter 1 is not a function" );
+    if (typeof callback !== "function") {
+      throw new TypeError("Failed to execute 'queueMicrotask': the callback provided as parameter 1 is not a function");
     }  
   };
 
-  if( typeof Promise === "function" && typeof Promise.resolve === "function" ) {
+  if (typeof Promise === "function" && typeof Promise.resolve === "function") {
     globalObj.queueMicrotask = (callback) => {
-      checkIsCallable( callback );
+      checkIsCallable(callback);
       Promise.resolve()
-        .then( () => callback() ) // call with no arguments
+        .then(() => callback()) // call with no arguments
         // if any error occurs during callback execution,
         // throw it back to globalObj (using setTimeout to get out of Promise chain)
-        .catch( (err) => setTimeout( () => { throw err; } ) );
+        .catch((err) => setTimeout(() => {throw err;}));
    };
   }
-  else if( typeof MutationObserver === 'function' ) {
+  else if (typeof MutationObserver === "function") {
     globalObj.queueMicrotask = (callback) => {
-      checkIsCallable( callback );
-      const observer = new MutationObserver( function() {
+      checkIsCallable(callback);
+      const observer = new MutationObserver(function() {
         callback();
         observer.disconnect();
-      } );
-      const target = document.createElement( 'div' );
-      observer.observe( target, { attributes: true } );
-      target.setAttribute( 'data-foo', '');
+      });
+      const target = document.createElement('div');
+      observer.observe(target, {attributes: true});
+      target.setAttribute('data-foo', '');
     };
   }
-  else if( typeof process === "object" && typeof process.nextTick === "function" ) {
+  else if (typeof process === "object" && typeof process.nextTick === "function") {
     globalObj.queueMicrotask = (callback) => {
-      checkIsCallable( callback );
-      process.nextTick( callback );
+      checkIsCallable(callback);
+      process.nextTick(callback);
     };
   }
   else {
     globalObj.queueMicrotask = (callback) => {
-      checkIsCallable( callback );
-      setTimeout( callback, 0 );
+      checkIsCallable(callback);
+      setTimeout(callback, 0);
     }
   }
 }
 })();
 
+queueMicrotask(() => console.log('microtask'));
+console.log('sync');
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replaceAll
+// implemented in Chrome 85
+// https://vanillajstoolkit.com/polyfills/stringreplaceall/
+if (!String.prototype.replaceAll) {
+  String.prototype.replaceAll = function(str, newStr) {
+    // If a regex pattern
+    if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+      return this.replace(str, newStr);
+    }
+    // If a string
+    return this.replace(new RegExp(str, 'g'), newStr);
+  };
+};
+// https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/replaceChildren
+// implemented in Chrome 86
+// https://github.com/XboxYan/dom-polyfill
+// copied from github-wc-polyfill
+(function() {
+  if (Element.prototype.replaceChildren === undefined) {
+    Element.prototype.replaceChildren = function(...nodesOrDOMStrings) {
+      while (this.lastChild) {
+        this.removeChild(this.lastChild)
+      }
+      if (nodesOrDOMStrings.length) {
+        this.append(...nodesOrDOMStrings)
+      }
+    }
+  }
+}());
 `;
 
 var script = document.createElement('script');
